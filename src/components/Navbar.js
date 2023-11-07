16f744a4a57450b3d6e93ab2f26b160aa5608c9d
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-07 10:22:19 +0300

    add header styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <header>
+    <header className="bg-[#2C2C38] border-b border-slate-500 p-6 flex items-center justify-between text-white">
       <h2>Platform Launch</h2>
-      <NavLink to='/add'>Add New Task</NavLink>
+      <NavLink to="/add" className="px-3 py-2 bg-[#645FC6] rounded-3xl">
+        + Add New Task
+      </NavLink>
     </header>
   )
 }
