fcacaef4f5a76aa6a6e4a0c1dea53d36eff3f333
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-07 10:23:20 +0300

    update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Task() {
   return (
-    <div>
-      
+    <div className="task">
+      <NavLink to="/:id" >
+        <h3 className="text-white font-semibold">Build UI for onboarding flow</h3>
+        <p>0 of 3 subtasks</p>
+      </NavLink>
     </div>
   )
 }
