3c5043e3136b2feb7b586a093c20741c27128ce6
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-07 12:30:10 +0300

    update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import AddTaskForm from './AddTaskForm'
+import { NavLink } from 'react-router-dom'
 
 export default function AddTask() {
   return (
-    <div>
-      
-    </div>
+    <section className="flex flex-col justify-center w-full text-white p-4 mt-6 mx-2 ">
+      <NavLink to="/" >
+        Back
+      </NavLink>
+      <AddTaskForm />
+    </section>
   )
 }
