4d2ce6e14d0b0e766ff1aa57b0ad34819296df1a
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-07 10:24:35 +0300

    add create, add and :id routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../layouts/Layout'
 import Tasks from './Tasks'
+import CreateBoard from './CreateBoard'
+import AddTask from './AddTask'
+import TaskDetail from './TaskDetail'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Tasks />}/>
+        <Route path='/create' element={<CreateBoard />}/>
+        <Route path='/add' element={<AddTask />}/>
+        <Route path='/:id' element={<TaskDetail />}/>
       </Route>
     </Routes>
   )
