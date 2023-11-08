db857169a62b846102087454a1c9c2d49af509b3
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-08 16:41:53 +0300

    map over data state to get all boards

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { BoardsContext } from '../layouts/Layout'
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false)
-  
+  const { data, currentBoard, setCurrentBoard } = useContext(BoardsContext)
+
   function handleClick(){
     setIsOpen(!isOpen)
   }
@@ -14,9 +16,11 @@ export default function Sidebar() {
         <h1 className="px-6 mb-6 text-2xl text-white font-medium">Kanban</h1>
         <h3 className="px-6 mb-3 text-neutral-400">ALL BOARDS (3)</h3>
         <ul className="text-neutral-400">
-          <li className="active">Platform Launch</li>
-          <li>Marketing Plan</li>
-          <li>Roadmap</li>
+          {data && data.map(board => {
+            return (
+              <li onClick={() => setCurrentBoard(board)} className={board === currentBoard? 'active' : ''}>{board.board_name}</li>
+            )
+          })}
         </ul>
         <NavLink to="/create" className="px-6 mt-3 text-[#645FC6]">
           + Create New Board
