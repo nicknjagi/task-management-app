9b1edec6b6c76a1ab6a8faf68b6aaf7a4e33cc61
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-08 16:46:11 +0300

    update TaskColumn.js

--- src/components/TaskColumn.js
+++ src/components/TaskColumn.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Task from './Task'
 
-export default function TaskColumn() {
+export default function TaskColumn({title, taskArr}) {
   return (
-    <section>
-      <h4 className='mb-6'>TODO (4)</h4>
-      <div className='flex flex-col gap-4'>
-        <Task />
-        <Task />
+    <section className="min-w-[240px] max-w-[240px]">
+      <h4 className="mb-6 uppercase tracking-widest text-sm">
+        {title} ({taskArr.length})
+      </h4>
+      <div className="flex flex-col gap-4">
+        {taskArr.map((task) => {
+          const doneSubtasks = task.subtasks.filter(
+            (el) => el.completed === true
+          ).length
+          return <Task task={task} doneSubtasks={doneSubtasks} />
+        })}
       </div>
     </section>
   )
