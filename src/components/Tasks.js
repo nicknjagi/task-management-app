58a07797801011cf598142abc41c404a2a023791
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-08 16:42:36 +0300

    update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import TaskColumn from './TaskColumn'
+import { BoardsContext } from '../layouts/Layout'
 
 export default function Tasks() {
+  const status = ["todo","doing","done"]
+  const {currentBoard} = useContext(BoardsContext)
+
   return (
-    <div>
-    </div>
+    <main className='tasks-container'>
+      {currentBoard && status.map(el => {
+        const newArr = currentBoard.board_tasks.filter(task => task.status === el)
+        return <TaskColumn taskArr={newArr} title={el}/>
+      })}
+    </main>
   )
 }
