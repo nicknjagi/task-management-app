835045f33d17a13da814b1e3b75f9fc01fe4c21c
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-08 16:43:17 +0300

    fetch data and create context

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import { createContext } from 'react'
+
+export const BoardsContext = createContext()
 
 export default function Layout() {
+  const [data, setData] = useState()
+  // const [isLoading, setIsLoading] = useState(true)
+  const [currentBoard, setCurrentBoard] = useState('')
+
+  useEffect(() => {
+    // setIsLoading(true)
+    fetch('https://task-management-app-ibvr.onrender.com/boards')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setCurrentBoard(data[0])
+      })
+    }, [])
+
   return (
-    <div className='container'>
-      <Sidebar />
-      <div className='w-full  border-l border-slate-500'>
-        <Navbar />
-        <Outlet />
+    <BoardsContext.Provider
+      value={{ data, currentBoard, setCurrentBoard }}>
+      <div className="container">
+        <Sidebar />
+        <div className="grid grid-rows-[100px_1fr] w-full">
+          <Navbar />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </BoardsContext.Provider>
   )
 }
