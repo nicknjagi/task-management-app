50565506ff0f8d02cd3055707c8ab77e8c7d6f8f
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-08 16:40:39 +0300

    update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import { NavLink } from 'react-router-dom'
+import { BoardsContext } from '../layouts/Layout'
 
 export default function Navbar() {
+  const {currentBoard} = useContext(BoardsContext)
+
   return (
-    <header className="bg-[#2C2C38] border-b border-slate-500 p-6 flex items-center justify-between text-white">
-      <h2>Platform Launch</h2>
+    <header className="bg-[#2C2C38] border-b border-slate-500 w-full p-6 flex items-center justify-between text-white">
+      <h2>{currentBoard.board_name}</h2>
       <NavLink to="/add" className="px-3 py-2 bg-[#645FC6] rounded-3xl">
         + Add New Task
       </NavLink>
