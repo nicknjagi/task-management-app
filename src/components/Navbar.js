a40e0b894e812f3fe77919e9e93659cc84396059
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-09 15:33:01 +0300

    update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,50 @@
 import React, { useContext} from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { BoardsContext } from '../layouts/Layout'
 
 export default function Navbar() {
-  const {currentBoard} = useContext(BoardsContext)
+  const { boards, currentBoard, setCurrentBoard } = useContext(BoardsContext)
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  function handleChange(e){
+    if (e.target.value === 'create'){
+      navigate('/create')
+      return
+    }
+    const curr = boards.filter(board => board.board_name === e.target.value)[0]
+    setCurrentBoard(curr)
+    navigate('/')
+  }
 
   return (
     <header className="bg-[#2C2C38] border-b border-slate-500 w-full p-6 flex items-center justify-between text-white">
-      <h2>{currentBoard.board_name}</h2>
-      <NavLink to="/add" className="px-3 py-2 bg-[#645FC6] rounded-3xl">
-        + Add New Task
+      <div>
+        <h2 className="hidden md:inline-block">{location.pathname === '/create' ? 'Create Board' :currentBoard.board_name}</h2>
+        <select
+          name="board"
+          id="board"
+          className="border-0 md:hidden"
+          value={location.pathname === '/create' ? 'Create Board' :currentBoard.board_name}
+          onChange={handleChange}>
+          {boards &&
+            boards.map((board) => {
+              return (
+                <option
+                  key={board.id}
+                  className={board === currentBoard ? 'bg-[#645FC6]' : ''}
+                  value={board.board_name}>
+                  {board.board_name}
+                </option>
+              )
+            })}
+          <option value="create">
+              + Create New Board
+          </option>
+        </select>
+      </div>
+      <NavLink to="/add" className="md:px-3 md:py-2 bg-[#645FC6] hover:bg-violet-800 rounded-3xl transition">
+        <span title='Add a task' className='p-4 md:p-0'> + </span> <span className="hidden md:inline">Add New Task</span>
       </NavLink>
     </header>
   )
