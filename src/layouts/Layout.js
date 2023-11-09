104651bb56dfb7c62967296926ab3c911ba917b8
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-09 20:09:09 +0300

    update Layout.js

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -9,6 +9,8 @@ export const BoardsContext = createContext()
 export default function Layout() {
   const [boards, setBoards] = useState('')
   const [currentBoard, setCurrentBoard] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://task-management-app-ibvr.onrender.com/boards')
@@ -19,9 +21,27 @@ export default function Layout() {
       })
     }, [])
 
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('https://task-management-app-ibvr.onrender.com/tasks')
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data.filter((task) => task.board_id === currentBoard.id))
+        setIsLoading(false)
+      })
+  }, [currentBoard])
+
   return (
     <BoardsContext.Provider
-      value={{ boards, currentBoard, setCurrentBoard }}>
+      value={{
+        boards,
+        currentBoard,
+        setCurrentBoard,
+        setBoards,
+        tasks,
+        setTasks,
+        isLoading
+      }}>
       <div className="container">
         <Sidebar />
         <div className="grid grid-rows-[100px_1fr] oveflow-x-auto w-full">
