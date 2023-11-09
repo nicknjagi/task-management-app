64f71573fe5a4bb87ef6157e86db064091cd6b00
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-09 08:02:10 +0300

    update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { BoardsContext } from '../layouts/Layout'
 
 export default function Task({task, doneSubtasks}) {
-
+  const {setCurrentTask} = useContext(BoardsContext)
   
   return (
-    <div className="task">
-      <NavLink to={`/${task.task_id}`} >
+    <div className="task" onClick={()=> setCurrentTask(task)}>
+      <NavLink to={`/${task.id}`} >
         <h3 className="text-white capitalize font-semibold">{task.title}</h3>
         <p>{doneSubtasks} of {task.subtasks.length} subtasks</p>
       </NavLink>
