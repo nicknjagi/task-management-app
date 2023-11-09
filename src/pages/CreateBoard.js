de02b3ad7fcc4d9fca534db8ab85218151253850
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-09 20:09:29 +0300

    update CreateBoard.js

--- src/pages/CreateBoard.js
+++ src/pages/CreateBoard.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { BoardsContext } from '../layouts/Layout'
 
 export default function CreateBoard() {
+  const [name, setName] = useState('')
+  const {boards, setBoards} = useContext(BoardsContext)
+  const navigate = useNavigate()
+  const MySwal = withReactContent(Swal) 
+
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if (name === '') return
+
+    fetch(`https://task-management-app-ibvr.onrender.com/boards`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ board_name:name}),
+    })
+    .then(res => res.json())
+    .then((data) => {
+      setBoards([...boards, data])
+      MySwal.fire({
+        text: 'Board created!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      }).then((res) => {
+        navigate('/')
+      })
+    })
+  }
   return (
-    <section className="flex items-center justify-center w-full">
-      
+    <section className="flex flex-col w-full text-white py-2 mt-6 mb-8 ">
+      <NavLink to="/" className="pl-6">
+        Back
+      </NavLink>
+      <form className="add-task-form max-w-[500px]">
+        <h2>Create new board</h2>
+        <div className="flex flex-col gap-6">
+          <div className="form-row">
+            <label htmlFor="board-name">Board name</label>
+            <input
+              type="text"
+              id="board-name"
+              name="board-name"
+              placeholder="e.g. User Engagement Survey"
+              value={name}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="create-task-btn bg-btn-purple hover:bg-violet-800 transition"
+            onClick={handleSubmit}>
+            Create
+          </button>
+        </div>
+      </form>
     </section>
   )
 }
