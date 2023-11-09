e2a1128967dafc3cc3fce0918bd73e07e1ce6b8c
Nicholas Njagi <njaginicholas@gmail.com>
2023-11-09 20:08:59 +0300

    update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,11 @@
-import React, { useContext, useEffect, useState} from 'react'
+import React, { useContext} from 'react'
 import TaskColumn from './TaskColumn'
 import Loader from './Loader'
 import { BoardsContext } from '../layouts/Layout'
 
 export default function Tasks() {
-  const [tasks, setTasks] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const {currentBoard} = useContext(BoardsContext)
+  const { tasks, isLoading } = useContext(BoardsContext)
   const status = ["todo","doing","done"]
-
-  useEffect(()=> {
-    setIsLoading(true)
-    fetch('https://task-management-app-ibvr.onrender.com/tasks')
-      .then((res) => res.json())
-      .then((data) => {
-        setTasks(data.filter((task) => task.board_id === currentBoard.id))
-        setIsLoading(false)
-      })
-  }, [currentBoard])
   
   if (isLoading) return <Loader />
   
