f30039f0eee16e3d6e053961adfac5a2b416b66f
Nick <njaginicholas@gmail.com>
2024-04-11 20:52:50 +0300

    move App.jsx

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
+import Tasks from './components/Tasks'
+import Layout from './layouts/Layout'
+import AddTask from './routes/AddTask'
+import CreateBoard from './routes/CreateBoard'
+import TaskDetail from './routes/TaskDetail'
+import { getBoards } from './features/board/boardSlice'
+
+function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getBoards())
+  },[])
+
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Tasks />}/>
+        <Route path='/create' element={<CreateBoard />}/>
+        <Route path='/add' element={<AddTask />}/>
+        <Route path='/:id' element={<TaskDetail />}/>
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
