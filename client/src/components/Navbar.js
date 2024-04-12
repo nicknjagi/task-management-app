44d08a823277b999886c5d3bc81931105b42de51
Nick <njaginicholas@gmail.com>
2024-04-12 23:08:37 +0300

    update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,88 +1,41 @@
-import React, { useContext, useState} from 'react'
+import React, { useState} from 'react'
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
-import { BoardsContext } from '../layouts/Layout'
 import elipsis from '../assets/images/ellipsis-vertical.svg'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import { useSelector } from 'react-redux'
 
 export default function Navbar() {
+  const {currentBoard, boards} = useSelector(state => state.board)
   const [showDelete, setShowDelete] = useState(false)
-  const { boards,setBoards, currentBoard, setCurrentBoard} = useContext(BoardsContext)
   const navigate = useNavigate()
   const location = useLocation()
-
-  const MySwal = withReactContent(Swal) 
-
-  function handleChange(e){
-    if (e.target.value === 'create'){
-      navigate('/create')
-      return
-    }
-    const curr = boards.filter(board => board.board_name === e.target.value)[0]
-    setCurrentBoard(curr)
-    navigate('/')
-  }
-
-  function handleClick() {
-    setShowDelete(!showDelete)
-  }
-
-  function deleteBoard(id){
-    MySwal.fire({
-      title: 'Do you want to delete the board?',
-      showCancelButton: true,
-      icon: 'warning',
-      confirmButtonText: 'Delete',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch(
-          `/api/boards/${id}`,
-          {
-            method: 'DELETE',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        ).then(() => {
-          Swal.fire('Board has been deleted!', '', 'success')
-          const newBoards = boards.filter((board) => board.id !== id)
-          setBoards(newBoards)
-          setCurrentBoard(newBoards[0])
-          setShowDelete(false)
-          })
-        }
-        else if(result.isDismissed){
-          setShowDelete(false)
-        }
-    })
-  }
 
   return (
     <header className="bg-[#2C2C38] border-b border-slate-500 w-full p-6 md:pl-10 flex items-center justify-between text-white">
       <div>
         <h2 className="hidden md:inline-block">
-          {location.pathname === '/create'
+          {location.pathname === '/board/create'
             ? 'Create Board'
-            : currentBoard.board_name}
+            : currentBoard.boardName}
         </h2>
         <select
           name="board"
           id="board"
           className="border-0 md:hidden"
           value={
-            location.pathname === '/create'
+            location.pathname === '/board/create'
               ? 'Create Board'
-              : currentBoard.board_name
+              : currentBoard.boardName
           }
-          onChange={handleChange}>
+          // onChange={handleChange}
+          >
           {boards &&
-            boards.map((board) => {
+            boards.boards.map((board) => {
               return (
                 <option
                   key={board.id}
                   className={board === currentBoard ? 'bg-[#645FC6]' : ''}
-                  value={board.board_name}>
-                  {board.board_name}
+                  value={board.boardName}>
+                  {board.boardName}
                 </option>
               )
             })}
@@ -91,7 +44,7 @@ export default function Navbar() {
       </div>
       <div className="relative flex gap-4 justify-between items-center">
         <NavLink
-          to="/add"
+          to="/task/add"
           className="md:px-3 md:py-2 bg-[#645FC6] hover:bg-violet-800 rounded-3xl transition">
           <span title="Add a task" className="p-4 md:p-0">
             {' '}
@@ -103,14 +56,16 @@ export default function Navbar() {
           type="button"
           className="w-6"
           aria-label="options"
-          onClick={handleClick}>
+          // onClick={handleClick}
+          >
           <img src={elipsis} alt="delete icon" />
         </button>
         {showDelete && (
           <button
             type="button"
             className="absolute right-3 -bottom-10 w-32 py-1 px-2 bg-red-500 hover:bg-red-600 rounded-md"
-            onClick={() => deleteBoard(currentBoard.id)}>
+            // onClick={() => deleteBoard(currentBoard.id)}
+            >
             Delete board
           </button>
         )}
