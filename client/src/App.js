63e426fce08d136a4a16966b1f3e50fa89fc2842
Nick <njaginicholas@gmail.com>
2024-04-17 20:58:42 +0300

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { Route, Routes } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Route, Routes, Navigate, useNavigate, useLocation } from 'react-router-dom'
+import { history } from './_helpers/history';
 import Tasks from './components/Tasks'
 import Layout from './layouts/Layout'
 import AddTask from './routes/AddTask'
 import CreateBoard from './routes/CreateBoard'
 import TaskDetail from './routes/TaskDetail'
 import { getBoards } from './features/board/boardSlice'
+import { getColumns } from './features/column/columnSlice'
 
 function App() {
+  const {currentBoard} = useSelector(state => state.board)
   const dispatch = useDispatch()
+
+  history.navigate = useNavigate();
+  history.location = useLocation();
 
   useEffect(()=>{
     dispatch(getBoards())
+    dispatch(getColumns(currentBoard?.id))
   },[])
 
+  useEffect(()=>{
+    dispatch(getColumns(currentBoard?.id))
+  },[currentBoard])
+  
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
