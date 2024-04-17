9c6f9a71e7c77de128c9b372d5def4fd00539756
Nick <njaginicholas@gmail.com>
2024-04-17 20:58:51 +0300

    update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import boardReducer from './features/board/boardSlice'
+import columnReducer from './features/column/columnSlice'
 
 export const store = configureStore({
     reducer: {
-        board:boardReducer
+        board:boardReducer,
+        column:columnReducer
     }
 })
