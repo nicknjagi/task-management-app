fd1166784d73493c1b4bec63297d6bc12614ed9e
Nick <njaginicholas@gmail.com>
2024-04-17 21:04:40 +0300

    update CreateBoard.js

--- client/src/routes/CreateBoard.js
+++ client/src/routes/CreateBoard.js
@@ -1,41 +1,43 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BackButton from '../components/BackButton'
+import { createBoard } from '../features/board/boardSlice'
+import { useDispatch } from 'react-redux'
 
 export default function CreateBoard() {
-  const [name, setName] = useState('')
+  const dispatch = useDispatch()
   const navigate = useNavigate()
-
-  function handleChange(e){
-    setName(e.target.value)
-  }
 
   function handleSubmit(e){
     e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const task = Object.fromEntries(formData.entries())
+    task['userId'] = 1
 
+    if(task.boardName !== ''){
+      dispatch(createBoard(task))
+    }
   }
+
   return (
     <section className="section">
       <BackButton />
-      <form className="add-task-form max-w-[500px]">
+      <form onSubmit={handleSubmit} className="add-task-form max-w-[500px]">
         <h2>Create new board</h2>
         <div className="flex flex-col gap-6">
           <div className="form-row">
-            <label htmlFor="board-name">Board name</label>
+            <label htmlFor="boardName">Board name</label>
             <input
               type="text"
-              id="board-name"
-              name="board-name"
+              id="boardName"
+              name="boardName"
               placeholder="e.g. User Engagement Survey"
-              value={name}
-              onChange={handleChange}
             />
           </div>
           <button
-            type="submit"
             className="create-task-btn bg-btn-purple hover:bg-violet-800 transition"
-            onClick={handleSubmit}>
-            Create
+          >
+            Create board
           </button>
         </div>
       </form>
