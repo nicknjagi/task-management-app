2ec22c9554bed6026bc23006106763b9d52dd6e6
Nick <njaginicholas@gmail.com>
2024-04-18 22:50:20 +0300

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Route, Routes, Navigate, useNavigate, useLocation } from 'react-router-dom'
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import { history } from './_helpers/history';
 import Tasks from './components/Tasks'
 import Layout from './layouts/Layout'
@@ -9,6 +9,7 @@ import CreateBoard from './routes/CreateBoard'
 import TaskDetail from './routes/TaskDetail'
 import { getBoards } from './features/board/boardSlice'
 import { getColumns } from './features/column/columnSlice'
+import UpdateBoard from './routes/UpdateBoard';
 
 function App() {
   const {currentBoard} = useSelector(state => state.board)
@@ -30,9 +31,6 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Tasks />}/>
-        <Route path='/board/create' element={<CreateBoard />}/>
-        <Route path='/task/add' element={<AddTask />}/>
-        <Route path='/task/:id' element={<TaskDetail />}/>
       </Route>
     </Routes>
   )
