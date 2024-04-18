51c7f4028742a1f5e4aac83849c0edd67f779dcf
Nick <njaginicholas@gmail.com>
2024-04-18 22:53:31 +0300

    update Layout.js

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import {Toaster} from 'react-hot-toast'
 
 export default function Layout() {
-  const [boards, setBoards] = useState('')
-  const [currentBoard, setCurrentBoard] = useState('')
-  const [tasks, setTasks] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  // const [boards, setBoards] = useState('')
+  // const [currentBoard, setCurrentBoard] = useState('')
+  // const [tasks, setTasks] = useState([])
+  // const [isLoading, setIsLoading] = useState(true)
 
   return (
       <div className="container">
+        <Toaster />
         <Sidebar />
-        <div className="grid grid-rows-[100px_1fr] oveflow-x-auto w-full">
+        <div className="flex flex-col overflow-x-auto w-full ">
           <Navbar />
           <Outlet />
         </div>
