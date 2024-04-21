e01a67eae7db5ccae998941cd5848511916350a2
Nick <njaginicholas@gmail.com>
2024-04-21 22:21:54 +0300

    update taskSlice.js

--- client/src/features/task/taskSlice.js
+++ client/src/features/task/taskSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { history } from "../../_helpers/history";
+// import { history } from "../../_helpers/history";
 import toast from "react-hot-toast";
 import { getColumns } from "../column/columnSlice";
 
@@ -9,12 +9,36 @@ const url = 'http://localhost:5000/api/v1/tasks'
 const initialState = {
     tasks:[],
     isLoading:false,
+    isUpdating:false,
+    isDeleting:false,
     currentTask:{}
 }
 
 export const createTask = createAsyncThunk('task/createTask', async (task,thunkAPI) => {
     try {
         const resp = await axios.post(url, task)
+        thunkAPI.dispatch(getColumns(thunkAPI.getState().board.currentBoard.id))
+        return resp.data
+    } catch (error) {
+        console.log(error);
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+}) 
+
+export const updateTask = createAsyncThunk('task/updateTask', async (task,thunkAPI) => {
+    try {
+        const resp = await axios.patch(`${url}/${task.id}`, task)
+        thunkAPI.dispatch(getColumns(thunkAPI.getState().board.currentBoard.id))
+        return resp.data
+    } catch (error) {
+        console.log(error);
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+}) 
+
+export const deleteTask = createAsyncThunk('task/deleteTask', async (id,thunkAPI) => {
+    try {
+        const resp = await axios.delete(`${url}/${id}`)
         thunkAPI.dispatch(getColumns(thunkAPI.getState().board.currentBoard.id))
         return resp.data
     } catch (error) {
@@ -37,12 +61,33 @@ export const taskSlice = createSlice({
         })
         .addCase(createTask.fulfilled, (state, action) => {
             state.isLoading = false
-            toast.success('Task has been created')
-            console.log(action.payload);
+            toast.success('Task created')
         })
         .addCase(createTask.rejected, (state, action) => {
             state.isLoading = false
             toast.error(action.payload)
+        })
+        .addCase(updateTask.pending, (state) => {
+            state.isUpdating = true
+        })
+        .addCase(updateTask.fulfilled, (state, action) => {
+            state.isUpdating = false
+            toast.success('Task updated')
+        })
+        .addCase(updateTask.rejected, (state, action) => {
+            state.isUpdating = false
+            toast.error(action.payload || 'something went wrong')
+        })
+        .addCase(deleteTask.pending, (state) => {
+            state.isDeleting = true
+        })
+        .addCase(deleteTask.fulfilled, (state, action) => {
+            state.isDeleting = false
+            toast.success('Task deleted')
+        })
+        .addCase(deleteTask.rejected, (state, action) => {
+            state.isDeleting = false
+            toast.error(action.payload || 'something went wrong')
         })
     }
 })
