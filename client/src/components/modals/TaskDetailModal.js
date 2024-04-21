cf6c3fbd9b7945fad324cb4a11130d0f1e420ad1
Nick <njaginicholas@gmail.com>
2024-04-21 22:22:39 +0300

    feat: create TaskDetailModal.js

---
+++ client/src/components/modals/TaskDetailModal.js
@@ -0,0 +1,39 @@
+import { useDispatch} from "react-redux";
+import TaskDetail from "../TaskDetail";
+import { setCurrentTask } from "../../features/task/taskSlice";
+import { useState } from "react";
+
+const TaskDetailModal = ({task }) => {
+  const dispatch = useDispatch()
+  const [doneSubtasks, setDoneSubtasks] = useState(task.subtasks.filter(
+    (el) => el.completed === true
+  ).length)
+
+  const handleClick = (task) => {
+    dispatch(setCurrentTask(task))
+    document.getElementById(`task-${task.id}`).showModal()
+  }
+
+  return (
+    <div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <div
+        role="button"
+        className="task"
+        onClick={() => handleClick(task)}
+      >
+        <h3 className="text-white capitalize font-semibold mb-2">{task.title}</h3>
+        <p className="text-sm font-bold">{doneSubtasks} of {task.subtasks.length} subtasks</p>
+      </div>
+      <dialog id={`task-${task.id}`} className="modal ">
+        <div className="modal-box">
+          <TaskDetail task={task} doneSubtasks={doneSubtasks} setDoneSubtasks={setDoneSubtasks}/>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
+  );
+};
+export default TaskDetailModal;
