520fb7fed6ca293565fb1b39a748dd8e01895220
Nick <njaginicholas@gmail.com>
2024-04-22 15:04:26 +0300

    update DeleteTaskModal.js

--- client/src/components/modals/DeleteTaskModal.js
+++ client/src/components/modals/DeleteTaskModal.js
@@ -16,15 +16,15 @@ const DeleteTaskModal = ({setShowOptions}) => {
         {/* Open the modal using document.getElementById('ID').showModal() method */}
         <button className='hover:bg-transparent text-left text-red hover:text-red-hover px-2 py-2 w-full ' onClick={()=>document.getElementById('delete-task').showModal()}>Delete Task</button>
         <dialog id="delete-task" className="modal">
-          <div className="modal-box">
-          <div className="p-6">
+          <div className="modal-box ">
+          <div className="p-6 bg-white dark:bg-dark-grey">
               <h3 className="font-bold text-lg text-red">Delete this task?</h3>
               <p className="my-6 text-sm text-mid-grey ">Are you sure you want to delete the ‘{currentTask?.title}’ task and its subtasks? This action cannot be reversed.</p>
               <div className="grid grid-cols-2 gap-4 ">
-                <button onClick={handleDelete} className="py-2 px-6 bg-red text-white hover:bg-red-hover rounded-[20px] font-bold transition">Delete</button>
+                <button onClick={handleDelete} className="delete-btn">Delete</button>
                 <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
-                <button onClick={()=>setShowOptions(false)} className="w-full dark:bg-white py-2 dark:text-main-purple dark:hover:text-main-purple-hover rounded-[20px] font-bold transition">Cancel</button>
+                <button onClick={()=>setShowOptions(false)} className="cancel-btn">Cancel</button>
                 </form>
               </div>
             </div>
