1f42e99b8e57f29ba3174d63a9c657fe2649e080
Nick <njaginicholas@gmail.com>
2024-04-22 20:52:41 +0300

    update ThemeToggle.js

--- client/src/components/ThemeToggle.js
+++ client/src/components/ThemeToggle.js
@@ -27,7 +27,7 @@ const ThemeToggle = () => {
     }
     
   return (
-    <div className='bg-light-grey dark:bg-very-dark-grey flex justify-center gap-6 py-4  rounded-md'>
+    <div className='bg-light-grey dark:bg-very-dark-grey flex justify-center gap-6 py-4 transition rounded-md'>
         <img className='w-5 h-5' src={light} alt="" />
         <button onClick={setTheme} className='flex items-center bg-main-purple hover:bg-main-purple-hover w-10 h-5 p-1 rounded-xl transition'>
             <span className={`block w-[14px] h-[14px] rounded-full bg-white transition ${lightTheme ? 'mr-auto' : 'ml-auto'}`}></span>
