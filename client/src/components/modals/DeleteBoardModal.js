2684b396dac63c6994cb8f70713fd243c596c48d
Nick <njaginicholas@gmail.com>
2024-04-22 15:04:34 +0300

    update DeleteBoardModal.js

--- client/src/components/modals/DeleteBoardModal.js
+++ client/src/components/modals/DeleteBoardModal.js
@@ -17,14 +17,14 @@ const DeleteBoardModal = ({handleClick}) => {
         <button onClick={()=>document.getElementById('delete-board').showModal()}className='hover:bg-transparent text-left text-red hover:text-red-hover px-2 py-2 w-full '>Delete board</button>
         <dialog id="delete-board" className="modal hover:bg-transparent">
         <div className="modal-box">
-            <div className="p-6">
+            <div className="p-6 bg-white dark:bg-dark-grey">
               <h3 className="font-bold text-lg text-red">Delete this board?</h3>
               <p className="my-6 text-sm text-mid-grey ">Are you sure you want to delete the ‘{currentBoard?.boardName}’ board? This action will remove all columns and tasks and cannot be reversed.</p>
               <div className="grid grid-cols-2 gap-4 ">
-                <button onClick={handleDelete} className="py-2 px-6 bg-red hover:bg-red-hover rounded-[20px] font-bold transition">Delete</button>
+                <button onClick={handleDelete} className="delete-btn">Delete</button>
                 <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
-                <button onClick={handleClick} className="w-full dark:bg-white py-2 dark:text-main-purple dark:hover:text-main-purple-hover rounded-[20px] font-bold transition">Cancel</button>
+                <button onClick={handleClick} className="cancel-btn">Cancel</button>
                 </form>
               </div>
             </div>
