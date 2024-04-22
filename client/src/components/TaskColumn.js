83e39815f6160b54a056735065eb7e82ca261d09
Nick <njaginicholas@gmail.com>
2024-04-22 15:04:42 +0300

    update TaskColumn.js

--- client/src/components/TaskColumn.js
+++ client/src/components/TaskColumn.js
@@ -4,7 +4,7 @@ import TaskDetailModal from './modals/TaskDetailModal'
 export default function TaskColumn({title, taskArr}) {
   return (
     <section className="min-w-[280px] max-w-[280px] ">
-      <h4 className="mb-6 uppercase tracking-widest text-sm">
+      <h4 className="mb-6 uppercase tracking-widest text-sm font-bold text-mid-grey">
         {title} ({taskArr.length})
       </h4>
       <div className="flex flex-col gap-4">
