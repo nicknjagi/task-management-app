3ea1d7a9eea8723ace7f4a14898cf4147cc08118
Nick <njaginicholas@gmail.com>
2024-04-24 11:22:02 +0300

    remove close button, enable close by clicking outside the component

--- client/src/components/modals/AddTaskModal.js
+++ client/src/components/modals/AddTaskModal.js
@@ -8,12 +8,12 @@ const AddTaskModal = () => {
         <button onClick={()=>document.getElementById('add-task').showModal()}className='text-white bg-main-purple hover:bg-main-purple-hover px-4 pb-2 md:py-2 text-2xl font-bold md:text-base rounded-full '><span>+</span> <span className="hidden md:inline ml-2 text-sm">Add New Task</span></button>
         <dialog id="add-task" className="modal">
             <div className="modal-box">
-                <form method="dialog" >
-                {/* if there is a button in form, it will close the modal */}
-                <button className="button button-circle button-ghost absolute text-xl right-8 top-6">✕</button>
-                </form>
                 <AddTaskForm />
             </div>
+            <form method="dialog" className="modal-backdrop">
+            {/* if there is a button in form, it will close the modal */}
+            <button>✕</button>
+            </form>
         </dialog>
     </div>
   )
