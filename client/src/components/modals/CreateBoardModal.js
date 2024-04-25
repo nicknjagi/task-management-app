46fb89f13f588c5acbaf2f6481e5d311924ed4c3
Nick <njaginicholas@gmail.com>
2024-04-25 20:13:47 +0300

    add modal close button for small screens

--- client/src/components/modals/CreateBoardModal.js
+++ client/src/components/modals/CreateBoardModal.js
@@ -13,6 +13,10 @@ const CreateBoardModal = () => {
         <dialog id="create-board" className="modal">
             <div className="modal-box ">
                 <CreateBoardForm />
+                <form method="dialog" className="md:hidden scale-125 absolute top-7 right-6">
+                {/* if there is a button in form, it will close the modal */}
+                <button>✕</button>
+                </form>
             </div>
             <form method="dialog" className="modal-backdrop">
             {/* if there is a button in form, it will close the modal */}
