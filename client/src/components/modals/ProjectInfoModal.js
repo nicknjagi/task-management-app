ee73768a1f70c512ed8733f022da713e9fa84cd3
Nick <njaginicholas@gmail.com>
2025-01-02 20:44:01 +0000

    update ProjectInfoModal.js

--- client/src/components/modals/ProjectInfoModal.js
+++ client/src/components/modals/ProjectInfoModal.js
@@ -1,24 +1,39 @@
-import {useState} from "react"
-import { BadgeInfo, CircleX } from "lucide-react";
+// import { useState } from "react";
+// import { BadgeInfo, CircleX } from "lucide-react";
 
 const ProjectInfoModal = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  // const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={`fixed right-3 bottom-4 ml-auto rounded ${isOpen ? 'w-auto overflow-visible' : 'overflow-hidden'}`} >
-      <div className={`flex items-center gap-2 p-2 max-w-[450px] text-sm text-dark-grey dark:text-light-grey transition duration-300 origin-right ${isOpen ? 'translate-x-0 shadow dark:shadow-lines-dark' : 'translate-x-[calc(100%-56px)]'}`}>
-      <button              
-          className="hover:dark:bg-main-purple-hover hover:bg-main-purple hover:bg-opacity-10 hover:dark:bg-opacity-10 p-2 flex justify-center items-center rounded"
-          onClick={()=>setIsOpen(!isOpen)}
-        >
-          {isOpen ? <CircleX color="#635fc7" /> : <BadgeInfo color="#635fc7" />}
-        </button>
-        
-        <p className={``}>
-        The backend server is hosted on a free tier platform, causing it to spin down when inactive, which can lead to a 50-second delay during the initial load.
-      </p>
-      </div>
-    </div>
+    <div></div>
+    // <div
+    //   className={`fixed bottom-4 mr-auto border border-red-400 rounded ${
+    //     isOpen
+    //       ? "w-full max-w-[450px] overflow-visible left-1/2 -translate-x-1/2 md:left-[calc[100%-24px] md:translate-x-0 bottom-4"
+    //       : "overflow-visible max-w-8 max-h-8 md:-left-[calc[100%-24px]"
+    //   }`}
+    // >
+    //   <div
+    //     className={`flex items-start justify-start gap-2 text-sm text-dark-grey dark:text-light-grey bg-white dark:bg-dark-grey transition duration-300 origin-right ${
+    //       isOpen ? "px-2 py-4 shadow dark:shadow-lines-dark translate-x-0" : "translate-x-0"
+    //     }`}
+    //   >
+    //     <button
+    //       className="w-full max-w-8 hover:dark:bg-main-purple-hover hover:bg-main-purple hover:bg-opacity-10 hover:dark:bg-opacity-10 rounded"
+    //       onClick={() => setIsOpen(!isOpen)}
+    //     >
+    //       {isOpen ? <CircleX color="#635fc7" /> : <BadgeInfo color="#635fc7" />}
+    //     </button>
+
+    //     <p
+    //       className={``}
+    //     >
+    //       The backend server is hosted on a free tier platform, causing it to
+    //       spin down when inactive, which can lead to a 50-second delay during
+    //       the initial load.
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 export default ProjectInfoModal;
